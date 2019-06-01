90351dfb6c47bf0ef3eb73d4fe5c4e6cc4ba5b17
stevenzack <stevenzack@qq.com>
2019-06-01 13:17:43 +0800

    Just a Backup

--- livedata.go
+++ livedata.go
@@ -1,38 +1,61 @@
 package livedata
 
-import "github.com/StevenZack/pubsub"
+import "fmt"
 
 const (
 	chanName = "livedata/"
 )
 
-var (
-	dataMap     = make(map[string]interface{})
-	dataMapChan = make(chan Data, 1)
-	ps          = pubsub.NewPubSub()
-)
-
 type Data struct {
 	ID   string
 	Data interface{}
+	Slot func(interface{})
 }
+
+var (
+	eventMap = make(map[string][]func(interface{}))
+	dataMap  = make(map[string]interface{})
+	add      = make(chan Data, 1)
+	remove   = make(chan Data, 1)
+	call     = make(chan Data, 1)
+)
 
 func init() {
 	go func() {
-		for i := range dataMapChan {
-			dataMap[i.ID] = i.Data
+		for {
+			select {
+			case data := <-add:
+				fns := eventMap[data.ID]
+				eventMap[data.ID] = append(fns, data.Slot)
+			case data := <-remove:
+				fns := eventMap[data.ID]
+				for index, fn := range fns {
+					if fmt.Sprint(fn) == fmt.Sprint(data.Slot) {
+						eventMap[data.ID] = append(fns[:index], fns[index+1:]...)
+						break
+					}
+				}
+			case data := <-call:
+				dataMap[data.ID] = data.Data
+				fns := eventMap[data.ID]
+				for _, fn := range fns {
+					fn(data.Data)
+				}
+			}
 		}
 	}()
 }
-func Observe(id string, f func(interface{})) {
-	go func() {
-		ps.Sub(chanName+id, f)
-	}()
+
+func Observe(id string, fn func(interface{})) {
+	add <- Data{ID: id, Slot: fn}
+}
+
+func Dismiss(id string, fn func(interface{})) {
+	remove <- Data{ID: id, Slot: fn}
 }
 
 func Set(id string, i interface{}) {
-	dataMapChan <- Data{ID: id, Data: i}
-	pubsub.Pub(chanName+id, i)
+	call <- Data{ID: id, Data: i}
 }
 
 func Get(id string) interface{} {
