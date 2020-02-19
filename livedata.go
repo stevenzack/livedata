73abc52485374d0a814c0417e564699c4a70cdb6
stevenzack <zhouyuhao@xiaoheiban.cn>
2020-02-19 19:32:24 +0800

    Just a Backup

--- livedata.go
+++ livedata.go
@@ -1,28 +1,35 @@
 package livedata
 
+import "github.com/StevenZack/pubsub"
+
 type LiveData struct {
-	value   interface{}
-	channel chan interface{}
+	value  interface{}
+	server *pubsub.Server
+	chanId string
 }
 
 func NewLiveData(v interface{}) *LiveData {
+	server := pubsub.NewServer()
+	server.Start()
 	return &LiveData{
-		value:   v,
-		channel: make(chan interface{}),
+		value:  v,
+		server: server,
+		chanId: "livedata",
 	}
 }
 
 func (l *LiveData) ObserveForever(onChange func(interface{})) {
 	go func() {
-		for v := range l.channel {
-			onChange(v)
-		}
+		client := pubsub.NewClient(l.server)
+		defer client.UnSub()
+		client.Sub(l.chanId, onChange)
 	}()
+	onChange(l.value)
 }
 
 func (l *LiveData) Post(v interface{}) {
 	l.value = v
-	l.channel <- v
+	l.server.Pub(l.chanId, v)
 }
 
 func (l *LiveData) Get() interface{} {
